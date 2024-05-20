8752ee08ef5dc8f24c17f23b8ee619348eb1a4b4
ismsahib <ismayilov.sahib.na@gmail.com>
2024-05-20 15:24:12 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
 import "./aliases";
+import fs from "fs";
 
-console.log("Hello world!");
+const parseFile = (fileName) =>
+  fs
+    .readFileSync(fileName, "utf8")
+    .split("\n")
+    .map((str) => str.trim().toLowerCase())
+    .filter((str) => str.length > 10);
+
+const collectData = () => {
+  const result: Record<string, string[]> = {};
+  for (let index = 5; index <= 46; index++) {
+    result[index.toString()] = parseFile(`src/DATA/${index}.txt`);
+  }
+  return result;
+};
+
+const merklyChecker = (wallets: string[]) => {
+  const result: Record<string, string[]> = {};
+  const data = collectData();
+
+  wallets.forEach((wallet) => {
+    for (const [key, value] of Object.entries(data)) {
+      if (value.includes(wallet)) {
+        if (result[key]) {
+          result[key] = [...result[key], wallet];
+        } else {
+          result[key] = [wallet];
+        }
+      }
+    }
+  });
+  return result;
+};
+
+console.log(merklyChecker(parseFile("src/wallets.txt")));
